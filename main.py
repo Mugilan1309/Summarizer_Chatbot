424268899149a3c48685eec6142b221c8fa6532a
Mugilan1309 <mugilanyoganandh@gmail.com>
2025-06-09 02:58:17 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,3 +1,6 @@
+import os
+os.environ["TOKENIZERS_PARALLELISM"] = "false"
+
 import streamlit as st
 from extractor.pdf_extractor import extract_text_from_pdf
 from summarizer.model_loader import load_summarizer_model
